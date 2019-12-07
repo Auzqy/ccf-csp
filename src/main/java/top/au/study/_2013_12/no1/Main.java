3c60bb5cc5d9e0287f948b38af37601f7c936fbe
au <auzhouqingyu@163.com>
2019-12-07 11:39:06 +0800

    201312-01 test pass

---
+++ src/main/java/top/au/study/_2013_12/no1/Main.java
@@ -0,0 +1,81 @@
+package top.au.study._2013_12.no1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * description:
+ *
+ * 试题编号：	201312-1
+ * 试题名称：	出现次数最多的数
+ * 时间限制：	1.0s
+ * 内存限制：	256.0MB
+ * 问题描述：
+ * 问题描述
+ * 　　给定n个正整数，找出它们中出现次数最多的数。如果这样的数有多个，请输出其中最小的一个。
+ * 输入格式
+ * 　　输入的第一行只有一个正整数n(1 ≤ n ≤ 1000)，表示数字的个数。
+ * 　　输入的第二行有n个整数s1, s2, …, sn (1 ≤ si ≤ 10000, 1 ≤ i ≤ n)。相邻的数用空格分隔。
+ * 输出格式
+ * 　　输出这n个次数中出现次数最多的数。如果这样的数有多个，输出其中最小的一个。
+ * 样例输入
+ * 6
+ * 10 1 10 20 30 20
+ * 样例输出
+ * 10
+ *
+ *
+ *
+ * createTime: 2019-12-07 10:50
+ *
+ * @author au
+ */
+public class Main {
+
+    private int n;
+    private int[] s;
+    private HashMap<Integer, Integer> numAndCount;
+
+    private void initData() {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        s = new int[n];
+        numAndCount = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            s[i] = scanner.nextInt();
+            int orDefault = numAndCount.getOrDefault(s[i], 0);
+            numAndCount.put(s[i], orDefault+1);
+        }
+    }
+
+    private void findMaxCountAndMinNum(HashMap<Integer, Integer> numAndCount) {
+        // 用于记录出现最多的次数
+        int maxCount = -1;
+        // 用于记录出现次数最多的元素对应的数字
+        int minNumAndMaxCount = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> entry : numAndCount.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if (value > maxCount) {
+                maxCount = value;
+                minNumAndMaxCount = key;
+            } else if (value == maxCount) {
+                if (key < minNumAndMaxCount) {
+                    minNumAndMaxCount = key;
+                }
+            }
+        }
+        System.out.print(minNumAndMaxCount);
+    }
+
+
+    public Main() {
+        initData();
+        findMaxCountAndMinNum(numAndCount);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
---
+++ src/test/java/top/au/study/_2013_12/no1/MainTest.java
@@ -0,0 +1,50 @@
+package top.au.study._2013_12.no1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+
+class MainTest {
+
+    static Stream<Arguments> intExpected_stringInput() {
+        return Stream.of(
+                Arguments.of("10","6\n" +
+                        "10 1 10 20 30 20")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("intExpected_stringInput")
+    void mainTest(String expectedOutput, String consoleInput) {
+
+        // 将原本从控制台输入的内容，输入到 ByteArrayInputStream
+        System.setIn(new ByteArrayInputStream(consoleInput.getBytes()));
+
+        // 分配空间
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+
+        // 将原本输出到控制台Console的字符流 重定向 到 bytes
+        System.setOut(new PrintStream(bytes));
+
+        //执行要测试的程序，eg. Main.main(new String[]{});
+        Main.main(new String[]{});
+
+        // bytes.toString() 作用是将 bytes内容 转换为字符流
+        /*
+            // 精确匹配
+            Assertions.assertEquals(expectedOutput,bytes.toString());
+            // 模糊匹配
+            Assert.assertThat(bytes.toString(), CoreMatchers.containsString(expectedOutput));
+            // 模糊匹配
+            MatcherAssert.assertThat(bytes.toString(), CoreMatchers.containsString(expectedOutput));
+         */
+         Assertions.assertEquals(expectedOutput,bytes.toString());
+    }
+
+}
