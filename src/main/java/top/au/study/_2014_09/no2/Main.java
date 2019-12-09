1956d515da65dfa1422808a05416cc55f62e3406
au <auzhouqingyu@163.com>
2019-12-09 11:39:00 +0800

    201409-02 pass

--- src/main/java/top/au/study/_2014_09/no2/Main.java
+++ src/main/java/top/au/study/_2014_09/no2/Main.java
@@ -1,8 +1,10 @@
 package top.au.study._2014_09.no2;
 
+import java.util.Scanner;
+
 /**
  * description:
- *
+ * <p>
  * 试题编号：	201409-2
  * 试题名称：	画图
  * 时间限制：	1.0s
@@ -11,7 +13,7 @@ package top.au.study._2014_09.no2;
  * 问题描述
  * 　　在一个定义了直角坐标系的纸上，画一个(x1,y1)到(x2,y2)的矩形指将横坐标范围从x1到x2，纵坐标范围从y1到y2之间的区域涂上颜色。
  * 　　下图给出了一个画了两个矩形的例子。第一个矩形是(1,1) 到(4, 4)，用绿色和紫色表示。第二个矩形是(2, 3)到(6, 5)，用蓝色和紫色表示。图中，一共有15个单位的面积被涂上颜色，其中紫色部分被涂了两次，但在计算面积时只计算一次。在实际的涂色过程中，所有的矩形都涂成统一的颜色，图中显示不同颜色仅为说明方便。
- *
+ * <p>
  * 　　给出所有要画的矩形，请问总共有多少个单位的面积被涂上颜色。
  * 输入格式
  * 　　输入的第一行包含一个整数n，表示要画的矩形的个数。
@@ -27,11 +29,61 @@ package top.au.study._2014_09.no2;
  * 评测用例规模与约定
  * 　　1<=n<=100，0<=横坐标、纵坐标<=100。
  *
- *
- *
+ * 满分通过
  *
  * createTime: 2019-12-08 09:50
+ *
  * @author au
  */
 public class Main {
+
+    /**
+     * 所有矩形的个数
+     */
+    private int n;
+
+    /**
+     * 整个盘面
+     * 染过的色单元记录为1，未染过色的记录为0（默认值）
+     */
+    private int[][] board;
+
+    /**
+     * 所有染色的个数
+     */
+    private int area;
+
+    private void initData() {
+        board = new int[110][110];
+
+        Scanner scanner = new Scanner(System.in);
+
+        n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
+
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
+                    if (board[j][k] != 1) {
+                        board[j][k] = 1;
+                        area++;
+                    }
+                }
+            }
+
+        }
+    }
+
+    public Main() {
+        initData();
+        System.out.print(area);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
 }
